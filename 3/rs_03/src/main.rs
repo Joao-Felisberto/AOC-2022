2bf4e5a5d6daed3fab7132622b1cb53f1b32961b
Joao-Felisberto <joaofelisberto@ua.pt>
2022-12-03 15:15:16 +0000

    day 3 part 2 rust solution

--- 3/rs_03/src/main.rs
+++ 3/rs_03/src/main.rs
@@ -2,9 +2,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn domain_hash(e: usize) -> usize {
-    if e >= 65 && e <= 90 { // upper
+    if e >= 65 && e <= 90 {
+        // upper
         e - (65 - 27) - 1
-    } else if e <= 122 {    // lower
+    } else if e <= 122 {
+        // lower
         e - 96 - 1
     } else {
         panic!("Invalid element!") // error, should never happen
@@ -12,14 +14,22 @@ fn domain_hash(e: usize) -> usize {
 }
 
 fn main() {
+    /*
+    // DEBUG 2510
     println!("{}", domain_hash('A' as usize));
     println!("{}", domain_hash('Z' as usize));
     println!("{}", domain_hash('a' as usize));
     println!("{}", domain_hash('z' as usize));
+    */
+
     let file = File::open("../input.txt").expect("Cannot read file.");
     let reader = BufReader::new(file);
 
     let mut score: usize = 0;
+
+    let mut badge_score: usize = 0;
+    let mut badge_hash_set: [u8; 52] = [0; 52];
+    let mut mod_3_cnt = 0;
     for (_, ln) in reader.lines().enumerate() {
         if let Ok(rucksack) = ln {
             // A -> 65  27
@@ -29,7 +39,19 @@ fn main() {
             let rucksack = rucksack.as_bytes();
             let mut left_hash_set = [false; 52]; //Vec::with_capacity(52);
             let mut right_hash_set = [false; 52];
-
+            /*
+            if mod_3_cnt == 2 {
+                for i in 0..badge_hash_set.len() {
+                    if badge_hash_set[i] == 3 {
+                        badge_score += i + 1;
+                        break;
+                    }
+                }
+                badge_hash_set = [0; 52];
+            }
+            mod_3_cnt = (mod_3_cnt + 1) % 3;
+            */
+            let mut j = 0;
             let half = rucksack.len() / 2;
             for i in 0..half {
                 let left = domain_hash(rucksack[i] as usize);
@@ -40,15 +62,43 @@ fn main() {
 
                 if right_hash_set[left] {
                     score += left + 1;
+                    j = i;
                     break;
                 }
                 if left_hash_set[right] {
                     score += right + 1;
+                    j = i;
                     break;
                 }
             }
+
+            for i in j..half {
+                let left = domain_hash(rucksack[i] as usize);
+                let right = domain_hash(rucksack[i + half] as usize);
+
+                left_hash_set[left] = true;
+                right_hash_set[right] = true;
+            }
+
+            for i in 0..badge_hash_set.len() {
+                if left_hash_set[i] || right_hash_set[i] {
+                    badge_hash_set[i] += 1;
+                }
+            }
+
+            if mod_3_cnt == 2 {
+                for i in 0..badge_hash_set.len() {
+                    if badge_hash_set[i] == 3 {
+                        badge_score += i + 1;
+                        break;
+                    }
+                }
+                badge_hash_set = [0; 52];
+            }
+            mod_3_cnt = (mod_3_cnt + 1) % 3;
         }
     }
 
-    println!("{score}");
+    println!("Score of misplaced items: {score}");
+    println!("Score of badges: {badge_score}");
 }
